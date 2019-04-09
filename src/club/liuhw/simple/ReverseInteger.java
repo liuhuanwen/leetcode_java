fd0984885287d137891097c47ff39cd35bc544e0
liuhw <liuhuansir@126.com>
2019-04-09 11:16:38 +0800

    7、整数反转

--- src/club/liuhw/simple/ReverseInteger.java
+++ src/club/liuhw/simple/ReverseInteger.java
@@ -17,7 +17,25 @@ package club.liuhw.simple;
  * 输出: 21
  * 注意:
  * <p>
- * 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+ * 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−2的31次方,  2的31次方 − 1]。
+ * 请根据这个假设，如果反转后整数溢出那么就返回 0。
  */
 public class ReverseInteger {
+
+    public static void main(String[] args) {
+        int x = 1534236469;
+        System.out.println(reverse(x));
+    }
+
+    private static int reverse(int x) {
+        int temp = 0;
+        while (x != 0) {
+            int rev = x % 10;
+            x = x / 10;
+            if (temp > Integer.MAX_VALUE / 10 || temp == Integer.MAX_VALUE / 10 && rev > 7) return 0;
+            if (temp < Integer.MIN_VALUE / 10 || temp == Integer.MIN_VALUE / 10 && rev < -8) return 0;
+            temp = temp * 10 + rev;
+        }
+        return temp;
+    }
 }
