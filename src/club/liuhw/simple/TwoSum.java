39747a89ccee524ddc9620f33f98a7c4a58482f7
liuhw <liuhuansir@126.com>
2019-04-09 10:49:43 +0800

    1、两数之和

--- src/club/liuhw/simple/TwoSum.java
+++ src/club/liuhw/simple/TwoSum.java
@@ -1,5 +1,9 @@
 package club.liuhw.simple;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
  * <p>
@@ -13,4 +17,34 @@ package club.liuhw.simple;
  * 所以返回 [0, 1]
  */
 public class TwoSum {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 7, 12, 34, 4};
+        int target = 46;
+        System.out.println(Arrays.toString(twoSumByMap(nums, target)));
+    }
+
+    private static int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalStateException("不存在这样的值");
+    }
+
+    private static int[] twoSumByMap(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int temp = target - nums[i];
+            if (map.containsKey(temp) && map.get(temp) != i) {
+                return new int[]{map.get(temp), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalStateException("不存在这样的值");
+    }
+
 }
